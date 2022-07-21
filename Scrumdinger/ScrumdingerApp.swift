7d62909485168f500791664ecbe3ec3a29548915
Colette M <58490154+coletteemurphy@users.noreply.github.com>
2022-07-21 16:49:13 +0100

    Add files via upload

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Colette Murphy (The Phoenix Group) on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
